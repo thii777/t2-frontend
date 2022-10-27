89c5f9d5df1e254c7bf8960c6254702ead04aa97
thiago carvalho <thiagocarvalhogalvao@gmail.com>
2022-10-26 22:14:09 -0300

    bug: add alt to deploy

--- src/components/organims/background/basicBackground.js
+++ src/components/organims/background/basicBackground.js
@@ -3,7 +3,7 @@
 export default function BasicBackground() {
   return (
     <div className="basic-background-container">
-      <img src="https://www.sicoob.com.br/documents/76966867/97715071/Banner_1544x783px_Credito-Consignado.jpg" width="100%" />
+      <img src="https://www.sicoob.com.br/documents/76966867/97715071/Banner_1544x783px_Credito-Consignado.jpg" width="100%" alt="Minha Figura"/>
     </div>
   );
 }
